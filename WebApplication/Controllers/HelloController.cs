cba903b3868030250d3da07455f707bcecaf2700
minhthuan059 <trinhminhthuan059@gmail.com>
2025-10-16 16:56:28 +0700

    Feat: Implement simple feature MVC

---
+++ WebApplication/Controllers/HelloController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace WebApplication.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+
+        [HttpGet]
+        [Route("hello")]
+        public ActionResult Index(string name, string email)
+        {
+            var user = new Models.User
+            {
+                Name = name ?? "Guest",
+                Email = email
+            };
+            return View(user);
+        }
+    }
+}
